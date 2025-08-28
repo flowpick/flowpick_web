a821e500093c6b20a9aea7123259f59a864eedd7
KingSeokJin <sjin7629@gmail.com>
2025-08-28 17:03:27 +0900

    test 커밋 3 : 보라색

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,6 +1,6 @@
 const Test = () => {
   return (
-    <div className="h-[240px] w-full bg-green-300 text-xl text-white">
+    <div className="h-[240px] w-full bg-purple-300 text-xl text-white">
       이 공간은 라우팅 테스트용 코드이며, 추후 루트페이지(홈 페이지) 개발 완료시 대체 예정입니다.
     </div>
   );
