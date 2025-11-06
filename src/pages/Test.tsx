2c09d3f7e341e0e3d0322c1a25f9653772815f53
KingSeokJin <sjin7629@gmail.com>
2025-11-07 02:06:17 +0900

    feat :  시작화면 꾸미기

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,5 +1,11 @@
+import Logo from '../shared/assets/logo.svg';
 const Test = () => {
-  return <div className="h-[240px] w-full bg-purple-300 text-xl text-white">Flow-Pick 입니다!</div>;
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <img src={Logo} alt="로고" className="h-[20%] w-[60%]" />
+      <p className="text-40px font-[700]">Flow-Pick 웹 도메인 입니다!</p>
+    </div>
+  );
 };
 
 export default Test;
